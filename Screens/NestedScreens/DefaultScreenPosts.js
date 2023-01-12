aeee9db3eb02d4aaaa24d2dbec1d779b7917be01
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-01-12 18:06:22 +0200

    fix 5

--- Screens/NestedScreens/DefaultScreenPosts.js
+++ Screens/NestedScreens/DefaultScreenPosts.js
@@ -43,7 +43,7 @@ export const DefaultScreenPosts = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-start",
   },
 });
 
